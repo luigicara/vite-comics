9c98449aa75b2f1bb5d814ae70bc589bcd1639c0
luigicara <luigi.caravelli98@gmail.com>
2022-12-13 16:47:37 +0100

    completed jumbotron and cards

---
+++ src/components/main_components/MainCategories.vue
@@ -0,0 +1,74 @@
+<script>
+export default {
+    data() {
+        return {
+            categoriesCard: [
+                {
+                    title: 'Digital Comics',
+                    img: '/img/buy-comics-digital-comics.png'
+                },
+
+                {
+                    title: 'DC Merchandise',
+                    img: '/img/buy-comics-merchandise.png'
+                },
+
+                {
+                    title: 'Subscription',
+                    img: '/img/buy-comics-subscriptions.png'
+                },
+
+                {
+                    title: 'Comic Shop Locator',
+                    img: '/img/buy-comics-shop-locator.png'
+                },
+
+                {
+                    title: 'DC Power Visa',
+                    img: '/img/buy-dc-power-visa.svg'
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <section id="categories">
+        <div class="container">
+            <div v-for="card in categoriesCard" class="categories-card">
+                <img :src="card.img" alt="img">
+                <span>{{ card.title.toUpperCase() }}</span>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use "../../styles/partials/_mixins.scss" as *;
+@use "../../styles/partials/_variables.scss" as *;
+
+#categories {
+    height: 8rem;
+    background-color: $primaryColor;
+
+    .container {
+        @include space-between;
+        padding: 1rem 0;
+        height: 100%;
+
+        .categories-card {
+            line-height: 3rem;
+            font-size: 0.7rem;
+            color: white;
+
+            img {
+                height: 3.5rem;
+                max-width: 3.5rem;
+                float: left;
+                margin-right: 1rem;
+            }
+        }
+    }
+}
+</style>
---
+++ src/components/main_components/MainSeriesCards.vue
@@ -0,0 +1,77 @@
+<script>
+import slidesJson from '../../assets/dc-comics.json'
+
+export default {
+    data() {
+        return {
+            slides: slidesJson
+        }
+    }
+}
+</script>
+
+<template>
+    <section class="jumbotron">
+        <img src="/img/jumbotron.jpg" alt="jumbotron">
+    </section>
+
+    <section class="slides">
+        <div class="container">
+            <section class="cards">
+                <div v-for="slide in slides" class="card">
+                    <div>
+                        <img :src="slide.thumb" alt="thumbnail">
+                    </div>
+
+                    <div class="series">
+                        {{ slide.series.toUpperCase() }}
+                    </div>
+                </div>
+            </section>
+        </div>
+    </section>
+
+</template>
+
+<style lang="scss" scoped>
+.jumbotron {
+    height: 400px;
+    overflow: hidden;
+
+    img {
+        width: 100%;
+        min-height: 100%;
+    }
+}
+
+.slides {
+    background-color: #1c1c1c;
+    padding: 3rem 0;
+
+    .cards {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 2rem 1rem;
+
+        .card {
+            max-width: calc((100% - 5rem) / 6);
+
+            div {
+                overflow: hidden;
+                max-height: 12rem;
+
+                img {
+                    max-width: 100%;
+                }
+            }
+
+            .series {
+                color: white;
+                margin: 1rem 0;
+                font-size: 0.9rem;
+            }
+
+        }
+    }
+}
+</style>
